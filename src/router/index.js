80fc9e2ce8c5de4949fd9746ca8247f8fd5f6b2f
superStar <xing960428@163.com>
2021-11-30 23:02:06 +0800

    添加登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Login from '@/views/login/login.vue'
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [{
+  path: '/login',
+  name: 'login',
+  component: Login
+}]
 
 const router = new VueRouter({
   routes
---
+++ src/views/login/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login-container">登录页面</div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.login-container {
+    font-size: 16px;
+}
+</style>
